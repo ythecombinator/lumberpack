b9882a1aa0ca9ec3f758484dccf46629e5e52e4d
Matheus Brasil <matheus.brasil10@gmail.com>
2015-07-23 20:43:02 -0300

    Start splitting our gulpfile into multiple files
    Our gulpfile.js is very big and I think it is worth splitting the tasks
    into separate files

--- gulpfile.js
+++ gulpfile.js
@@ -1,124 +1,22 @@
-// For development => gulp
-// For production  => gulp -p
+var gulp = require('gulp');
 
-// Call Plugins
-var env         = require('minimist')(process.argv.slice(2)),
-  gulp        = require('gulp'),
-  gutil       = require('gulp-util'),
-  plumber     = require('gulp-plumber'),
-  jade        = require('gulp-jade'),
-  browserify  = require('gulp-browserify'),
-  browserSync = require('browser-sync'),
-  uglify      = require('gulp-uglify'),
-  concat      = require('gulp-concat'),
-  gulpif      = require('gulp-if'),
-  stylus      = require('gulp-stylus'),
-  stylint     = require('gulp-stylint'),
-  jeet        = require('jeet'),
-  rupture     = require('rupture'),
-  koutoSwiss  = require('kouto-swiss'),
-  prefixer    = require('autoprefixer-stylus'),
-  modRewrite  = require('connect-modrewrite'),
-  imagemin    = require('gulp-imagemin'),
-  karma       = require('gulp-karma'),
-  rsync       = require('rsyncwrapper').rsync;
+function getTask(task) {
+    return require('./tasks/' + task)(gulp);
+}
 
-// Call Jade for compile Templates
-gulp.task('jade', function(){
-  return gulp.src('src/templates/*.jade')
-    .pipe(plumber())
-    .pipe(jade({pretty: !env.p }))
-    .pipe(gulp.dest('build/'))
-});
+// Other tasks
+gulp.task('browserify', getTask('browserify'));
+gulp.task('browser-sync', getTask('browser-sync'));
+gulp.task('deploy', getTask('deploy'));
+gulp.task('imagemin', getTask('imagemin'));
+gulp.task('jade', getTask('jade'));
+gulp.task('js', getTask('js'));
+gulp.task('stylint', getTask('stylint'));
+gulp.task('stylus', getTask('stylus'));
 
-// Call Uglify and Concat JS
-gulp.task('js', function(){
-  return gulp.src('src/js/**/*.js')
-    .pipe(plumber())
-    .pipe(concat('main.js'))
-    .pipe(gulpif(env.p, uglify()))
-    .pipe(gulp.dest('build/js'))
-});
-
-// Call Uglify and Concat JS
-gulp.task('browserify', function(){
-  return gulp.src('src/js/main.js')
-    .pipe(plumber())
-    .pipe(browserify({debug: !env.p }))
-    .pipe(gulpif(env.p, uglify()))
-    .pipe(gulp.dest('build/js'))
-});
-
-// Call Stylus
-gulp.task('stylus', function(){
-    gulp.src('src/styles/main.styl')
-    .pipe(plumber())
-    .pipe(stylus({
-      use:[koutoSwiss(), prefixer(), jeet(),rupture()],
-      compress: env.p
-    }))
-    .pipe(gulp.dest('build/css'))
-});
-
-// Call Stylus Linter
-gulp.task('stylint', function () {
-  gulp.src('src/styles/**/*.styl')
-  .pipe(stylint({config: '.stylintrc'}))
-});
-
-// Call Imagemin
-gulp.task('imagemin', function() {
-  return gulp.src('src/images/**/*')
-    .pipe(plumber())
-    .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
-    .pipe(gulp.dest('build/img'));
-});
-
-// Call Watch
-gulp.task('watch', function(){
-  gulp.watch('src/templates/**/*.jade', ['jade']);
-  gulp.watch('src/styles/**/*.styl', ['stylint', 'stylus']);
-  gulp.watch('src/js/**/*.js', ['js']);
-  gulp.watch('src/images/**/*.{jpg,png,gif}', ['imagemin']);
-});
-
-// Call Watch for Browserify
-gulp.task('watchfy', function(){
-  gulp.watch('src/templates/**/*.jade', ['jade']);
-  gulp.watch('src/styles/**/*.styl', ['stylint', 'stylus']);
-  gulp.watch('src/js/**/*.js', ['browserify']);
-  gulp.watch('src/images/**/*.{jpg,png,gif}', ['imagemin']);
-});
-
-gulp.task('browser-sync', function () {
-   var files = [
-      'build/**/*.html',
-      'build/css/**/*.css',
-      'build/img/**/*',
-      'build/js/**/*.js'
-   ];
-
-   browserSync.init(files, {
-      server: {
-         baseDir: './build/'
-      }
-   });
-});
-
-// Rsync
-gulp.task('deploy', function(){
-  rsync({
-    ssh: true,
-    src: './build/',
-    dest: 'user@hostname:/path/to/www',
-    recursive: true,
-    syncDest: true,
-    args: ['--verbose']
-  },
-    function (erro, stdout, stderr, cmd) {
-      gutil.log(stdout);
-  });
-});
+// Watching tasks
+gulp.task('watch', getTask('watch'));
+gulp.task('watch', getTask('watchfy'));
 
 // Default task
 gulp.task('default', ['js', 'jade', 'stylint', 'stylus', 'imagemin', 'watch', 'browser-sync']);
