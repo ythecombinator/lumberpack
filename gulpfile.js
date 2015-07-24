875f2295ca71f6887fa53be7793269935b052d46
Matheus Brasil <matheus.brasil10@gmail.com>
2015-07-24 00:51:20 -0300

    Update tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ function getTask(task) {
 }
 
 // Other tasks
-gulp.task('browserify', getTask('browserify'));
 gulp.task('browser-sync', getTask('browser-sync'));
 gulp.task('deploy', getTask('deploy'));
 gulp.task('imagemin', getTask('imagemin'));
@@ -14,18 +13,11 @@ gulp.task('js', getTask('js'));
 gulp.task('stylint', getTask('stylint'));
 gulp.task('stylus', getTask('stylus'));
 
-// Watching tasks
+// Watching task
 gulp.task('watch', getTask('watch'));
-gulp.task('watch', getTask('watchfy'));
 
 // Default task
 gulp.task('default', ['js', 'jade', 'stylint', 'stylus', 'imagemin', 'watch', 'browser-sync']);
 
-// Default task using browserify
-gulp.task('fy', ['browserify', 'jade', 'stylint', 'stylus', 'imagemin', 'watchfy', 'browser-sync']);
-
 // Build and Deploy
 gulp.task('build', ['js', 'jade', 'stylint', 'stylus', 'imagemin', 'deploy']);
-
-// Build and Deploy
-gulp.task('buildfy', ['browserify', 'jade', 'stylint', 'stylus', 'imagemin', 'deploy']);
