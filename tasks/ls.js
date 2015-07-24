fdf6b3eda4ce4b9c86e90333eda4a66417db7407
Matheus Brasil <matheus.brasil10@gmail.com>
2015-07-24 01:46:21 -0300

    Update ls task

--- tasks/ls.js
+++ tasks/ls.js
@@ -1,13 +1,13 @@
 'use strict';
 
 // Necessary Plugins
-var env        = require('minimist')(process.argv.slice(2))
-  ,plumber     = require('gulp-plumber')
-  ,uglify      = require('gulp-uglify')
-  ,concat       = require('gulp-concat')
-  ,gulpLiveScript = require('gulp-livescript')
-  ,gutil       = require('gulp-util')
-  ,gulpif      = require('gulp-if');
+var env             = require('minimist')(process.argv.slice(2))
+  ,plumber          = require('gulp-plumber')
+  ,uglify           = require('gulp-uglify')
+  ,concat           = require('gulp-concat')
+  ,gulpLiveScript   = require('gulp-livescript')
+  ,gutil            = require('gulp-util')
+  ,gulpif           = require('gulp-if');
 
 // Call Livescript Compiler
 module.exports = function (gulp) {
