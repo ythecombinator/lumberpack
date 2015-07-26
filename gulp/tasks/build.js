0a7fa859c4501ea2376e3dfd5b42fe9e0c94822d
Matheus Brasil <matheus.brasil10@gmail.com>
2015-07-26 00:17:28 -0300

    Add comments

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,4 +2,5 @@
 
 var gulp = require('gulp');
 
+// Build and Deploy
 module.exports = gulp.task('build', ['ls', 'jade', 'stylint', 'stylus', 'imagemin', 'deploy']);
--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -2,4 +2,5 @@
 
 var gulp = require('gulp');
 
+// Default task
 module.exports = gulp.task('default', ['ls', 'jade', 'stylint', 'stylus', 'imagemin', 'watch', 'browser-sync']);
