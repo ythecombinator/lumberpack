ef2e13b3fc546ec0e2352d57c6e9c243db693485
Matheus Brasil <matheus.brasil10@gmail.com>
2015-07-29 02:06:53 -0300

    Update jade task to render jade from json data

--- gulp/tasks/jade.js
+++ gulp/tasks/jade.js
@@ -4,13 +4,19 @@
 var gulp     = require('gulp')
     ,plumber = require('gulp-plumber')
     ,env     = require('minimist')(process.argv.slice(2))
-    ,paths = require('../paths')
+    ,paths   = require('../paths')
+    ,path    = require('path')
+    ,fs      = require('fs')
+    ,data    = require('gulp-data')
     ,jade    = require('gulp-jade');
 
 // Call Jade to compile Templates
 module.exports = gulp.task('jade', function () {
-  return gulp.src(paths.source.jade)
+  return gulp.src(paths.source.files.jade)
     .pipe(plumber())
+    .pipe(data(function() {
+      return JSON.parse(fs.readFileSync('./src/config.json'));
+    }))
     .pipe(jade({pretty: !env.p }))
     .pipe(gulp.dest(paths.build.html))
 });
