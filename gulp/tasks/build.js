17261d3a9af61811bf2f535a775b02b492cc2f33
Matheus Brasil <matheus.brasil10@gmail.com>
2015-08-05 01:32:09 -0300

    Update build task

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -3,4 +3,4 @@
 var gulp = require('gulp');
 
 // Build and Deploy
-module.exports = gulp.task('build', ['ls', 'jade', 'stylint', 'stylus', 'imagemin', 'deploy']);
+module.exports = gulp.task('build', ['ls', 'jade', 'stylint', 'stylus', 'imagemin', 'deploy-rsync']);
